a15c0e810c4c73869ab74753b125ec248dbf0f09
Masato Kiryu <kiri9120@gmail.com>
2019-10-11 18:10:02 +0900

    create sample component files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SampleComponent/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import SampleComponent from '@/components/SampleComponent.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    SampleComponent
   }
 }
 </script>
